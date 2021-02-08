c68f41bf7fe9b0ce888da7bc84fcbf98ee3ed5f2
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-02-08 16:54:05 +0530

    Hey There

--- GitTestProject/Automate.sh
+++ GitTestProject/Automate.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env sh
 commitMessage="Hey There"
-echo git status
+currentBranch="UC1HeyThere"
+echo ---------------git status-------------------
+echo 
 git status
-echo git add *
+echo ---------------git add *---------------------
+echo 
 git add *
-echo git commit -m $commitMessage
+echo ---------------git commit -m $commitMessage---
+echo 
 git commit -m "$commitMessage"
+echo ---------------git push -m $commitMessage---
+echo 
+git push remote $currentBranch

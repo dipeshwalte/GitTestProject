22d8c31e93691237a12f1972a6f14dfb10502b15
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-02-08 17:01:19 +0530

    Hey There

--- GitTestProject/Automate.sh
+++ GitTestProject/Automate.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env sh
 commitMessage="Hey There"
-echo git status
+echo --------git status---------
+echo
 git status
-echo git add *
+echo ---------git add *--------------
+echo
 git add *
-echo git commit -m $commitMessage
+echo ------------git commit -m $commitMessage-----------------
+echo
 git commit -m "$commitMessage"
+echo ------------git push----------------
+echo
+git push

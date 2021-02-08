301e934ed2f0a8f7f7e0195bd9fa18da7b93ff2a
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-02-08 17:39:46 +0530

    [Dipesh] modify added1moreline

--- GitTestProject/Automate.sh
+++ GitTestProject/Automate.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
-commitMessage="Hey There"
+commitMessage="[Dipesh] modify added1moreline"
+currentbranchName="UC2HeyDone"
+newBranchName="UC3HeyDone1"
 echo --------git status---------
 echo
 git status
@@ -9,6 +11,17 @@ git add *
 echo ------------git commit -m $commitMessage-----------------
 echo
 git commit -m "$commitMessage"
-echo ------------git push----------------
+echo ------------git push-------------------
 echo
-git push -u origin master
+git remote show origin
+git push -u origin $currentbranchName
+echo ------------git checkout Master-------------------
+echo
+git checkout master
+echo ------------git pull-------------------
+echo
+git pull
+echo ------------git branch and checkout-------------------
+echo
+git branch $newBranchName
+git checkout $newBranchName
--- GitTestProject/Program.cs
+++ GitTestProject/Program.cs
@@ -7,6 +7,7 @@ namespace GitTestProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("1 More Line");
         }
     }
 }
